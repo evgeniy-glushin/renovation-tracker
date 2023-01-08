ec06008c56c9266e1d6cc3aba6ce319b071ebefa
yevhenh <yevhenh@varyence.com>
2023-01-08 14:23:33 +0200

    Initial Commit

---
+++ RenoTracker/RenoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenoTrackerApp.swift
+//  RenoTracker
+//
+//  Created by User on 08.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct RenoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
